02db72aa91fcaced1c0765e402ad3ca36d5187ef
Miguel Rodrigues <miguel.d.rodrigues@tecnico.ulisboa.pt>
2025-10-23 23:54:24 +0100

    corrected convention for Player and PlayerType classes

--- lib/core/player/player.dart
+++ lib/core/player/player.dart
@@ -2,18 +2,18 @@
 import 'package:casino_app/core/player/player_type.dart';
 
 class Player{
-  late int _id_player;
+  late int _idPlayer;
   late String _username;
   late String _email;
   late String _password;
-  PlayerType _player_type = PlayerType.NORMAL;
+  PlayerType _playerType = PlayerType.NORMAL;
   int _bankroll = 0;
-  int _session_money = 0; // maybe remove this
-  int _total_profit = 0;
-  int _total_money_betted = 0;
+  int _sessionMoney = 0; // maybe remove this
+  int _totalProfit = 0;
+  int _totalMoneyBetted = 0;
 
   Player(int id_player, String username, String email, String password){
-    _id_player = id_player;
+    _idPlayer = id_player;
     _username = username;
     _email = email;
     _password = password;
@@ -21,6 +21,6 @@ class Player{
 
   @override
   String toString() {
-    return "$_id_player:$_username:$_email:$_bankroll:$_player_type:$_total_profit";
+    return "$_idPlayer:$_username:$_email:$_bankroll:$_playerType:$_totalProfit";
   }
 }
