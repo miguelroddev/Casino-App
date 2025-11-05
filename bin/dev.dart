8dc504f4c3d7896585608609b9476b2e41b97740
Miguel Rodrigues <miguelrodriguesdev@outlook.com>
2025-11-05 11:46:21 +0000

    I fixed a bug with the dependencies of the pure dart console main, now the hive is fully working

--- bin/dev.dart
+++ bin/dev.dart
@@ -5,20 +5,22 @@ import 'package:casino_app/app/main_menu.dart';
 import 'package:casino_app/core/casino.dart';
 import 'package:casino_app/core/casino_manager.dart';
 import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 import 'package:casino_app/core/models/casino_model.dart' as model;
 import 'package:casino_app/core/config.dart';
 
 Future<void> main() async {
   final dir = Directory.current.path; // for console mode
   Hive.init(dir);
-  // 2. Register adapters
   Hive
     ..registerAdapter(model.DateAdapter())
     ..registerAdapter(model.PlayerTypeAdapter())
     ..registerAdapter(model.PlayerAdapter())
     ..registerAdapter(model.CasinoAdapter());
   isConsoleMode = true;
-  CasinoManager casinoManager = CasinoManager(Casino());
+  CasinoManager? casinoManager = await CasinoManager.loadCasino();
+  casinoManager ??= CasinoManager(Casino());
   MainMenu().start(casinoManager);
+  await casinoManager.saveCasino();
+  await Hive.close();
 }
